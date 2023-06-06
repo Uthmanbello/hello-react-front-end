2137574a19b46219830585491e52e159c453e75e
Uthmanbello <uthmanbello88@gmail.com>
2023-06-06 22:39:20 +0100

    Fix linter errors

--- src/actions/greetingActions.js
+++ src/actions/greetingActions.js
@@ -1,20 +1,14 @@
 import axios from 'axios';
 
-export const getRandomGreeting = () => {
-  return (dispatch) => {
-    axios.get('http://localhost:3000/greeting')
-      .then((response) => {
-        dispatch(setRandomGreeting(response.data.greeting));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-};
+export const setRandomGreeting = (greeting) => ({
+  type: 'SET_RANDOM_GREETING',
+  payload: greeting,
+});
 
-export const setRandomGreeting = (greeting) => {
-  return {
-    type: 'SET_RANDOM_GREETING',
-    payload: greeting,
-  };
+export const getRandomGreeting = () => (dispatch) => {
+  axios.get('http://localhost:3000/greeting')
+    .then((response) => {
+      dispatch(setRandomGreeting(response.data.greeting));
+    })
+    .catch((error) => error);
 };
